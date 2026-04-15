c40e9f5f389e2cbf454c4db621f3db60fb5362af
mberger-lang <m.berger@fergyholding.com>
2026-04-15 17:47:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
